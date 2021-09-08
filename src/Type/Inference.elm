f3e6b47168a3f70268303e1c540c937088a929e0
mathiajusth <matus.ledenyi@gmail.com>
2021-09-08 20:47:36 +0200

    update

--- src/Type/Inference.elm
+++ src/Type/Inference.elm
@@ -3,27 +3,106 @@ module Type.Inference exposing (..)
 import Context exposing (Context)
 import Type exposing (Type)
 import Type.Substitutions exposing (Substitutions)
+import Value exposing (Value)
 
 
-type alias State =
-    { ctx : Context
+type alias Context =
+    { ctx : Context.Context
     , subs : Substitutions
-    , varsCount : Int
+    , varPrefix : List Int
     }
 
 
-type alias Inference =
-    State -> ( State, Type )
-
-
-emptyState : State
-emptyState =
-    { ctx = Context.empty
-    , subs = Type.Substitutions.empty
-    , varsCount = 0
-    }
+type alias Inference a =
+    Context -> ( Context, a )
 
 
 
+-- Inf (Inf a)
+--   -> Context -> (Context, Inference a)
+--   -> Context -> (Context, Context -> (Context, a))
+
+
+type Term
+    = Leaf TermSymbol
+    | Tuple Tuple
+
+
+type Tuple
+    = Introduction Term Term
+    | Elimination Elimination
+
+
+type Elimination
+    = ProjL
+    | ProjR
+
+
+type alias TermSymbol =
+    String
+
+
+
+-- Applicative
+
+
+apply : Inference (a -> b) -> Inference a -> Inference b
+apply infAtoB infA =
+    -- which state update to do first?
+    -- how to define this based on monad?
+    \ctx ->
+        let
+            ( newCtx, aToB ) =
+                infAtoB ctx
+
+            ( newestCtx, a ) =
+                infA newCtx
+        in
+        ( newestCtx, aToB a )
+
+
+
+-- Monad
+
+
+applyInKleisli : (a -> Inference b) -> Inference a -> Inference b
+applyInKleisli kleisliFunction kleisliArgument =
+    \ctx ->
+        let
+            ( newCtx, a ) =
+                kleisliArgument ctx
+
+            infB =
+                kleisliFunction a
+        in
+        infB newCtx
+
+
+andThen : Inference a -> (a -> Inference b) -> Inference b
+andThen kleisliArgument kleisliFunction =
+    applyInKleisli kleisliFunction kleisliArgument
+
+
+
+-- andMap : Inference a -> Inference (a -> b) -> Inference b
+-- andMap infA infAtoB =
+--   \ctx ->
+-- freshVar : Inference TermSymbol
+-- freshVar =
+--   \ctx ->
+--     (
+--       )
+-- infer : Term -> Inference Type
+-- infer term =
+--   case term of
+--     Leaf termSymbol ->
+--       \ctx ->
+--         ({ctx | })
+-- emptyState : State
+-- emptyState =
+--     { ctx = Context.empty
+--     , subs = Type.Substitutions.empty
+--     , varsCount = 0
+--     }
 -- pure : a ->  Inference a
 -- pure =
