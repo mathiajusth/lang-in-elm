ae0e6145f7fe42b5ca25d7f73dce473c4f893199
mathiajusth <matus.ledenyi@gmail.com>
2021-09-09 23:58:18 +0200

    StatefulFallible applicative and monad

--- src/Data/Stateful/Fallible.elm
+++ src/Data/Stateful/Fallible.elm
@@ -36,10 +36,62 @@ map aToB statefulA state =
 
 
 
--- mapErr : (e -> f) -> Stateful state e a -> Stateful state f a
--- mapErr eToF statefulA state =
---     let
---         ( newState, a ) =
---             statefulA state
---     in
---     ( newState, aToB a )
+-- Applicative
+
+
+apply : StatefulFallible e state (a -> b) -> StatefulFallible e state a -> StatefulFallible e state b
+apply statefulAtoB statefulA =
+    -- which state update to do first?
+    -- how to define this based on monad?
+    \state ->
+        let
+            ( newState, resultAToB ) =
+                statefulAtoB state
+
+            ( newestState, resultA ) =
+                statefulA newState
+        in
+        ( newestState
+        , Result.andMap resultA resultAToB
+        )
+
+
+
+-- Monad
+
+
+applyInKleisli : (a -> StatefulFallible e state b) -> StatefulFallible e state a -> StatefulFallible e state b
+applyInKleisli kleisliFunction statefulA state =
+    let
+        ( newState, resultA ) =
+            statefulA state
+    in
+    Result.unpack (\err -> ( newState, Err err ))
+        (\a ->
+            kleisliFunction a newState
+        )
+        resultA
+
+
+andThen : (a -> StatefulFallible e state b) -> StatefulFallible e state a -> StatefulFallible e state b
+andThen =
+    applyInKleisli
+
+
+andThen2 : (a -> b -> StatefulFallible e state c) -> StatefulFallible e state a -> StatefulFallible e state b -> StatefulFallible e state c
+andThen2 f statefulA statefulB state =
+    let
+        ( newState, resultA ) =
+            statefulA state
+
+        ( newerState, resultB ) =
+            statefulB newState
+    in
+    Result.map2
+        (\a b ->
+            f a b newerState
+        )
+        resultA
+        resultB
+        |> Result.unpack (\err -> ( newState, Err err ))
+            identity
